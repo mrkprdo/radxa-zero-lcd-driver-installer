b14242a200321259c7554185a8c80eafc7377764
Mark Anthony Prado <mrkprdo@gmail.com>
2022-10-07 03:45:03 +0000

    working st7789 from HejinYo

--- fbtft/fb_st7789v.c
+++ fbtft/fb_st7789v.c
@@ -1,8 +1,17 @@
-// SPDX-License-Identifier: GPL-2.0+
 /*
  * FB driver for the ST7789V LCD Controller
  *
  * Copyright (C) 2015 Dennis Menschel
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 #include <linux/bitops.h>
@@ -19,12 +28,6 @@
 #define DEFAULT_GAMMA \
 	"70 2C 2E 15 10 09 48 33 53 0B 19 18 20 25\n" \
 	"70 2C 2E 15 10 09 48 33 53 0B 19 18 20 25"
-
-#define HSD20_IPS_GAMMA \
-	"D0 05 0A 09 08 05 2E 44 45 0F 17 16 2B 33\n" \
-	"D0 05 0A 09 08 05 2E 43 45 0F 16 16 2B 33"
-
-#define HSD20_IPS 1
 
 /**
  * enum st7789v_command - ST7789V display controller commands
@@ -82,67 +85,36 @@ enum st7789v_command {
  */
 static int init_display(struct fbtft_par *par)
 {
-	/* turn off sleep mode */
-	write_reg(par, MIPI_DCS_EXIT_SLEEP_MODE);
-	mdelay(120);
-
-	/* set pixel format to RGB-565 */
-	write_reg(par, MIPI_DCS_SET_PIXEL_FORMAT, MIPI_DCS_PIXEL_FMT_16BIT);
-	if (HSD20_IPS)
-		write_reg(par, PORCTRL, 0x05, 0x05, 0x00, 0x33, 0x33);
-
-	else
-		write_reg(par, PORCTRL, 0x08, 0x08, 0x00, 0x22, 0x22);
-
-	/*
-	 * VGH = 13.26V
-	 * VGL = -10.43V
-	 */
-	if (HSD20_IPS)
-		write_reg(par, GCTRL, 0x75);
-	else
-		write_reg(par, GCTRL, 0x35);
-
-	/*
-	 * VDV and VRH register values come from command write
-	 * (instead of NVM)
-	 */
-	write_reg(par, VDVVRHEN, 0x01, 0xFF);
-
-	/*
-	 * VAP =  4.1V + (VCOM + VCOM offset + 0.5 * VDV)
-	 * VAN = -4.1V + (VCOM + VCOM offset + 0.5 * VDV)
-	 */
-	if (HSD20_IPS)
-		write_reg(par, VRHS, 0x13);
-	else
-		write_reg(par, VRHS, 0x0B);
-
-	/* VDV = 0V */
-	write_reg(par, VDVS, 0x20);
-
-	/* VCOM = 0.9V */
-	if (HSD20_IPS)
-		write_reg(par, VCOMS, 0x22);
-	else
-		write_reg(par, VCOMS, 0x20);
-
-	/* VCOM offset = 0V */
-	write_reg(par, VCMOFSET, 0x20);
-
-	/*
-	 * AVDD = 6.8V
-	 * AVCL = -4.8V
-	 * VDS = 2.3V
-	 */
-	write_reg(par, PWCTRL1, 0xA4, 0xA1);
-
-	write_reg(par, MIPI_DCS_SET_DISPLAY_ON);
-
-	if (HSD20_IPS)
-		write_reg(par, MIPI_DCS_ENTER_INVERT_MODE);
-
-	return 0;
+    par->fbtftops.reset(par);
+    mdelay(50);
+    write_reg(par,0x11);//Sleep exit
+    mdelay(12);
+    write_reg(par,0x11);
+    mdelay(10);
+    write_reg(par,0x3A,0x05); //65k mode
+    write_reg(par,0xc5,0x1a);
+    write_reg(par,0x36,0x70); // 屏幕显示方向设置
+//-------------ST7789V Frame rate setting-----------//
+    write_reg(par,0xb2,0x05,0x05,0x00,0x33,0x33);
+    write_reg(par,0xb7,0x35);
+//--------------ST7789V Power setting---------------//
+    write_reg(par,0xbb,0x3f);
+    write_reg(par,0xc0,0x2c);
+    write_reg(par,0xc2,0x01);
+    write_reg(par,0xc3,0x0f);
+    write_reg(par,0xc4,0x20);
+    write_reg(par,0xc6,0x11);
+    write_reg(par,0xd0,0xa4,0xa1);
+    write_reg(par,0xe8,0x03);
+    write_reg(par,0xe9,0x09,0x09,0x08);
+    write_reg(par,0xe0,0xd0,0x05,0x09,0x09,0x08,0x14,0x28,0x33,0x3f,0x07,0x13,0x14,0x28,0x30);
+    write_reg(par,0xe1,0xd0,0x05,0x09,0x09,0x08,0x03,0x24,0x32,0x32,0x3b,0x14,0x13,0x28,0x2f);
+    write_reg(par,0x21);
+    write_reg(par,0x11);
+    mdelay(120);      //Delay 120ms
+    write_reg(par,0x29);
+    mdelay(200);
+    return 0;
 }
 
 /**
@@ -191,47 +163,48 @@ static int set_var(struct fbtft_par *par)
  *
  * Return: 0 on success, < 0 if error occurred.
  */
-static int set_gamma(struct fbtft_par *par, u32 *curves)
-{
-	int i;
-	int j;
-	int c; /* curve index offset */
-
-	/*
-	 * Bitmasks for gamma curve command parameters.
-	 * The masks are the same for both positive and negative voltage
-	 * gamma curves.
-	 */
-	static const u8 gamma_par_mask[] = {
-		0xFF, /* V63[3:0], V0[3:0]*/
-		0x3F, /* V1[5:0] */
-		0x3F, /* V2[5:0] */
-		0x1F, /* V4[4:0] */
-		0x1F, /* V6[4:0] */
-		0x3F, /* J0[1:0], V13[3:0] */
-		0x7F, /* V20[6:0] */
-		0x77, /* V36[2:0], V27[2:0] */
-		0x7F, /* V43[6:0] */
-		0x3F, /* J1[1:0], V50[3:0] */
-		0x1F, /* V57[4:0] */
-		0x1F, /* V59[4:0] */
-		0x3F, /* V61[5:0] */
-		0x3F, /* V62[5:0] */
-	};
-
-	for (i = 0; i < par->gamma.num_curves; i++) {
-		c = i * par->gamma.num_values;
-		for (j = 0; j < par->gamma.num_values; j++)
-			curves[c + j] &= gamma_par_mask[j];
-		write_reg(par, PVGAMCTRL + i,
-			  curves[c + 0],  curves[c + 1],  curves[c + 2],
-			  curves[c + 3],  curves[c + 4],  curves[c + 5],
-			  curves[c + 6],  curves[c + 7],  curves[c + 8],
-			  curves[c + 9],  curves[c + 10], curves[c + 11],
-			  curves[c + 12], curves[c + 13]);
-	}
-	return 0;
-}
+//static int set_gamma(struct fbtft_par *par, unsigned long *curves)
+//{
+//	int i;
+//	int j;
+//	int c; /* curve index offset */
+//
+//	/*
+//	 * Bitmasks for gamma curve command parameters.
+//	 * The masks are the same for both positive and negative voltage
+//	 * gamma curves.
+//	 */
+//	const u8 gamma_par_mask[] = {
+//		0xFF, /* V63[3:0], V0[3:0]*/
+//		0x3F, /* V1[5:0] */
+//		0x3F, /* V2[5:0] */
+//		0x1F, /* V4[4:0] */
+//		0x1F, /* V6[4:0] */
+//		0x3F, /* J0[1:0], V13[3:0] */
+//		0x7F, /* V20[6:0] */
+//		0x77, /* V36[2:0], V27[2:0] */
+//		0x7F, /* V43[6:0] */
+//		0x3F, /* J1[1:0], V50[3:0] */
+//		0x1F, /* V57[4:0] */
+//		0x1F, /* V59[4:0] */
+//		0x3F, /* V61[5:0] */
+//		0x3F, /* V62[5:0] */
+//	};
+//
+//	for (i = 0; i < par->gamma.num_curves; i++) {
+//		c = i * par->gamma.num_values;
+//		for (j = 0; j < par->gamma.num_values; j++)
+//			curves[c + j] &= gamma_par_mask[j];
+//		write_reg(
+//			par, PVGAMCTRL + i,
+//			curves[c + 0], curves[c + 1], curves[c + 2],
+//			curves[c + 3], curves[c + 4], curves[c + 5],
+//			curves[c + 6], curves[c + 7], curves[c + 8],
+//			curves[c + 9], curves[c + 10], curves[c + 11],
+//			curves[c + 12], curves[c + 13]);
+//	}
+//	return 0;
+//}
 
 /**
  * blank() - blank the display
@@ -250,17 +223,48 @@ static int blank(struct fbtft_par *par, bool on)
 	return 0;
 }
 
+
+static void fbtft_reset(struct fbtft_par *par)
+{
+    if (!par->gpio.reset)
+        return;
+    fbtft_par_dbg(DEBUG_RESET, par, "%s()\n", __func__);
+    gpiod_set_value_cansleep(par->gpio.reset, 1);
+    msleep(10);
+    gpiod_set_value_cansleep(par->gpio.reset, 0);
+    msleep(200);
+    gpiod_set_value_cansleep(par->gpio.reset, 1);
+    msleep(10);
+}
+
+static void set_addr_win(struct fbtft_par *par, int xs, int ys, int xe, int ye)
+{
+    xs += 40, xe += 40;
+    ys += 53, ye += 53;
+
+    write_reg(par, MIPI_DCS_SET_COLUMN_ADDRESS,
+              xs >> 8, xs & 0xFF, xe >> 8, xe & 0xFF);
+
+    write_reg(par, MIPI_DCS_SET_PAGE_ADDRESS,
+              ys >> 8, ys & 0xFF, ye >> 8, ye & 0xFF);
+
+    write_reg(par, MIPI_DCS_WRITE_MEMORY_START);
+}
+
+
 static struct fbtft_display display = {
 	.regwidth = 8,
 	.width = 240,
-	.height = 320,
+	.height = 135,
 	.gamma_num = 2,
 	.gamma_len = 14,
-	.gamma = HSD20_IPS_GAMMA,
+	.gamma = DEFAULT_GAMMA,
 	.fbtftops = {
+        .reset = fbtft_reset,
 		.init_display = init_display,
 		.set_var = set_var,
-		.set_gamma = set_gamma,
+		// .set_gamma = set_gamma,
+        .set_addr_win = set_addr_win,
 		.blank = blank,
 	},
 };
